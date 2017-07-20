56b394bfc4a0f9419c9e66a79244e3210961fde2
Steven Oh <stevenoh0908@gmail.com>
2017-07-20 19:55:51 +0900

    v 0.0.1 / Inlt
    초기화
    2017. 07. 20.

---
+++ app/src/main/java/org/thunderdot/bartender/SingleSelectGame.java
@@ -0,0 +1,13 @@
+package org.thunderdot.bartender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleSelectGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_select_game);
+    }
+}
